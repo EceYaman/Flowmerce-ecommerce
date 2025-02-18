2637beb1bd19fdf2087cbdc83b39292fabab9db4
Ece Yaman <eceymn35@gmail.com>
2025-02-18 23:48:13 +0300

    clientAction and clientReducer added

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import clientReducer from './clientReducer';
+
+
+const rootReducer = combineReducers({
+  client: clientReducer,
+ 
+});
+
+export default rootReducer;
