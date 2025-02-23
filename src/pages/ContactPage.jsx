19235ef6815a656acefd74a1b183b0e0ef781f80
Ece Yaman <eceymn35@gmail.com>
2025-02-24 00:11:50 +0300

    ContactPage desktop view created

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,10 +1,10 @@
 export function ContactPage() {
     return(
         <div className="px-8">
-            <div className="max-w-[65%] flex flex-col mx-auto text-center gap-10 my-10">
+            <div className="max-w-[65%] flex flex-col mx-auto text-center gap-10 my-10 md:max-w-[30%]">
                 <h3 className="title">Get answers to all your questions.</h3>
                 <p className="text-gray-text text-base font-medium">Problems trying to resolve the conflict between the two major realms of Classical physics:</p>
-                <button className="btn bg-primary">CONTACT OUR COMPANY</button>
+                <button className="btn bg-primary md:max-w-[60%] md:mx-auto">CONTACT OUR COMPANY</button>
                 <div className="flex justify-center gap-10">
                     <img src="./twitter.svg"/>
                     <img src="./facebook.svg"/>
