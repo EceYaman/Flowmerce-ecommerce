e0684a1a32a830ab31248a82466b3783efac4f69
Ece Yaman <eceymn35@gmail.com>
2025-02-24 00:08:10 +0300

    ContactPage mobile view created

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,16 @@
 export function ContactPage() {
     return(
         <div className="px-8">
-            Contact 
+            <div className="max-w-[65%] flex flex-col mx-auto text-center gap-10 my-10">
+                <h3 className="title">Get answers to all your questions.</h3>
+                <p className="text-gray-text text-base font-medium">Problems trying to resolve the conflict between the two major realms of Classical physics:</p>
+                <button className="btn bg-primary">CONTACT OUR COMPANY</button>
+                <div className="flex justify-center gap-10">
+                    <img src="./twitter.svg"/>
+                    <img src="./facebook.svg"/>
+                    <img src="./instagram.svg"/>
+                </div>
+            </div>
         </div>
     )
 }
