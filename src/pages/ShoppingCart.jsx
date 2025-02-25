cf8b4b31b6e843fdf48e658a70329790df393464
Ece Yaman <eceymn35@gmail.com>
2025-02-25 14:43:55 +0300

    Implement "add to cart" functionality and create shopping cart

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,7 +1,29 @@
+import { useSelector } from "react-redux";
+
 export function ShoppingCartPage() {
+    const cart = useSelector((state) => state.shoppingCart.cart);
     return(
-        <div className="px-8">
-            Shopping Cart
-        </div>
+        <div className="px-8 py-4">
+      <h1 className="text-2xl font-bold mb-4">Shopping Cart</h1>
+      {cart.length === 0 ? (
+        <p>No products in cart.</p>
+      ) : (
+        <ul>
+          {cart.map((item) => (
+            <li key={item.product.id} className="mb-4 border-b pb-2">
+              <div className="flex justify-between items-center">
+                <div>
+                  <h2 className="font-semibold">{item.product.name}</h2>
+                  <p>Quantity: {item.count}</p>
+                </div>
+                <div>
+                  <p>{item.product.price}₺</p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
     )
 }
--- src/store/actions/shoppingCartActions.js
+++ src/store/actions/shoppingCartActions.js
@@ -1,6 +1,7 @@
 export const SET_CART = 'shoppingCart/SET_CART';
 export const SET_PAYMENT = 'shoppingCart/SET_PAYMENT';
 export const SET_ADDRESS = 'shoppingCart/SET_ADDRESS';
+export const ADD_TO_CART = 'shoppingCart/ADD_TO_CART';
 
 export const setCart = (cart) => ({
   type: SET_CART,
@@ -15,4 +16,9 @@ export const setPayment = (payment) => ({
 export const setAddress = (address) => ({
   type: SET_ADDRESS,
   payload: address,
+});
+
+export const addToCart = (product) => ({
+  type: ADD_TO_CART,
+  payload: product,
 });
